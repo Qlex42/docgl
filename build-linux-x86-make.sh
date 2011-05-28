4f27765eef17dce03520c1c35550b569cc6c2330
Alexandre Buge <alexandre.buge@gmail.com>
2011-05-28 20:24:05 +0200

    cmake fix for ubuntu

--- build-linux-x86-make.sh
+++ build-linux-x86-make.sh
@@ -5,11 +5,11 @@ export CXXFLAGS=-m32
 export CFLAGS=-m32
 mkdir -p build/linux-x86-make/debug
 cd        build/linux-x86-make/debug
-../../../extern/bin/linux-x86/cmake/bin/ccmake -DCMAKE_BUILD_TYPE:String=Debug -DCMAKE_BUILD_ARCH=32 ../../..
+ccmake -DCMAKE_BUILD_TYPE:String=Debug -DCMAKE_BUILD_ARCH=32 ../../..
 make
 cd ../../..
 mkdir -p build/linux-x86-make/release
 cd        build/linux-x86-make/release
-../../../extern/bin/linux-x86/cmake/bin/ccmake -DCMAKE_BUILD_TYPE:String=Release -DCMAKE_BUILD_ARCH=32 ../../..
+ccmake -DCMAKE_BUILD_TYPE:String=Release -DCMAKE_BUILD_ARCH=32 ../../..
 make
 cd ../../..
